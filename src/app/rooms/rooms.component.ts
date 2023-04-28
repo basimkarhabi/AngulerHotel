690b1a6f707553517ca02dd3dd46efd3376614eb
basimkarhabi <basim.karhabi@gmail.com>
2023-04-28 15:51:30 +0200

    use input output to transfer data

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -12,6 +12,7 @@ export class RoomsComponent implements OnInit {
   hotelName = "Orients Hotel"
   numberOfRooms = 10;
   hiddeRooms = false;
+  selectedRoom!: | RoomList
 
   rooms: Room = {
     totalRooms: 20,
@@ -19,48 +20,55 @@ export class RoomsComponent implements OnInit {
     bookRooms: 10,
   }
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Delux Room',
-      amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
-      price: 500,
-      photos: "",
-      checkinTime: new Date('11-Nov-2023'),
-      checkoutTime: new Date('15-Nov-2023'),
-      rating: 4.5,
-
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Single Room',
-      amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
-      price: 1000,
-      photos: "",
-      checkinTime: new Date('11-Nov-2023'),
-      checkoutTime: new Date('15-Nov-2023'),
-      rating: 3.4,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Doupple Room',
-      amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
-      price: 1500,
-      photos: "",
-      checkinTime: new Date('11-Nov-2023'),
-      checkoutTime: new Date('15-Nov-2023'),
-      rating: 2.6,
-    }
-
-  ]
+  roomList: RoomList[] = []
 
   constructor() {
 
   }
-  ngOnInit(): void { }
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Delux Room',
+        amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
+        price: 500,
+        photos: "",
+        checkinTime: new Date('11-Nov-2023'),
+        checkoutTime: new Date('15-Nov-2023'),
+        rating: 4.5,
+
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Single Room',
+        amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
+        price: 1000,
+        photos: "",
+        checkinTime: new Date('11-Nov-2023'),
+        checkoutTime: new Date('15-Nov-2023'),
+        rating: 3.4,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Doupple Room',
+        amenities: 'Air Condition , Free Wifi , Tv ; Bathroom , Kitchen',
+        price: 1500,
+        photos: "",
+        checkinTime: new Date('11-Nov-2023'),
+        checkoutTime: new Date('15-Nov-2023'),
+        rating: 2.6,
+      }
+
+    ]
+  }
 
   toggle() {
+
     this.hiddeRooms = !this.hiddeRooms;
   }
 
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room
+  }
 }
